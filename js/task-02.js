2b22e9cabaa1418d5ff21c9875db72605d4f8c33
Oleksii-Kukharuk <104523043+Oleksii-Kukharuk@users.noreply.github.com>
2022-09-08 18:06:37 +0300

    second task done

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,23 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const newListElements = [];
+
+ingredients.forEach((element) => {
+  const newLi = document.createElement("li");
+  newLi.textContent = `${element}`;
+  newLi.classList.add("item");
+  // console.log(element);
+  return newListElements.push(newLi);
+});
+console.log(newListElements);
+
+const ingredientsRef = document.querySelector("#ingredients");
+console.log(ingredientsRef);
+ingredientsRef.append(...newListElements);
